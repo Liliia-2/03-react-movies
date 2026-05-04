843aa8c4593117869c6926dcca454f066b252ae2
Liliia <plv-86@ukr.net>
2026-05-04 23:47:08 +0300

    fourth commit

--- src/components/App/MovieModal/MovieModal.tsx
+++ src/components/App/MovieModal/MovieModal.tsx
@@ -32,25 +32,23 @@ export default function MovieModal({ movie, onClose }: MovieModalProps) {
     return createPortal(
         <div className={css.backdrop} onClick={handleBackdrop}>
 
-            {/* </div> role="dialog" aria-modal="true"> */}
             <div className={css.modal}>
                 <button className={css.closeButton} onClick={onClose}>
-                    {/* aria-label="Close modal"> */}
                     &times;
                 </button>
                 <img
                     src="https://image.tmdb.org/t/p/original/backdrop_path"
-                    alt="movie_title"
+                    alt={movie.title}
                     className={css.image}
                 />
                 <div className={css.content}>
-                    <h2>movie_title</h2>
-                    <p>movie_overview</p>
+                    <h2>{movie.title}</h2>
+                    <p>{movie.overview}</p>
                     <p>
-                        <strong>Release Date:</strong> movie_release_date
+                        <strong>Release Date:</strong> {movie.release_date}
                     </p>
                     <p>
-                        <strong>Rating:</strong> movie_vote_average/10
+                        <strong>Rating:</strong> {movie.vote_average}/10
                     </p>
                 </div>
             </div>
